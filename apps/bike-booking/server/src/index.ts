b782a5674980850f40f9777d4c466707c7a80d60
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-05-06 12:45:19 +0100

    Set up environment variable configuration using dotenv

--- apps/bike-booking/server/src/index.ts
+++ apps/bike-booking/server/src/index.ts
@@ -1,5 +1,8 @@
 import app from "./app";
+import * as dotenv from "dotenv";
 
-app.listen(5001, () => {
-  console.log("Server running on port 5001");
+dotenv.config();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
