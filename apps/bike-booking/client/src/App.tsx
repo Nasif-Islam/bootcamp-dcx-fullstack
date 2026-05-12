a1abce4cd9f1d58d2cfc02b82d6bf3d7b63d4cb9
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-05-12 11:54:55 +0100

    Imported and added the BrowseBikes component to App.tsx

--- apps/bike-booking/client/src/App.tsx
+++ apps/bike-booking/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import BrowseBikes from "./pages/BrowseBikes";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,6 +16,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <BrowseBikes />
     </>
   );
 }
