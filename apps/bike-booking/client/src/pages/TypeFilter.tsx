01034bf7fd745b308e4b281d619b71b8b8e1f31b
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-05-13 10:59:43 +0100

    Created a Type Filter component

---
+++ apps/bike-booking/client/src/pages/TypeFilter.tsx
@@ -0,0 +1,17 @@
+import "./TypeButton.css";
+
+const TypeFilter = () => {
+  return (
+    <div>
+      <div className="filter-container">
+        <span className="type-btn">All</span>
+        <span className="type-btn">Mountain</span>
+        <span className="type-btn">Road</span>
+        <span className="type-btn">City</span>
+        <span className="type-btn">Electric</span>
+      </div>
+    </div>
+  );
+};
+
+export default TypeFilter;
