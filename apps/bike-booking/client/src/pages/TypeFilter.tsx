c674268341b58836f5a4452fbbb63f9e6c4bc822
lisaosinowo1 <lisa.osinowo@capgemini.com>
2026-05-14 11:19:06 +0100

    Removed the commented out code, added explicit type to the types array

--- apps/bike-booking/client/src/pages/TypeFilter.tsx
+++ apps/bike-booking/client/src/pages/TypeFilter.tsx
@@ -1,25 +1,11 @@
 import "./TypeButton.css";
 
-// const TypeFilter = () => {
-//   return (
-//     <div>
-//       <div className="filter-container">
-//         <button type="button" className="type-btn">All</button>
-//         <button type="button" className="type-btn">Mountain</button>
-//         <button type="button" className="type-btn">Road</button>
-//         <button type="button" className="type-btn">City</button>
-//         <button type="button" className="type-btn">Electric</button>
-//       </div>
-//     </div>
-//   );
-// };
-
 interface TypeFilterProps {
   selectedType: string | null;
   onSelectType: (type: string | null) => void;
 }
 
-const types = ["mountain", "road", "city", "electric"];
+const types:string[] = ["mountain", "road", "city", "electric"];
 
 const TypeFilter = ({ selectedType, onSelectType }: TypeFilterProps) => {
   return (
